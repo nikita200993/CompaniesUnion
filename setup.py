840f47637b44f22d6d530e8c1be671980661cfaf
NikitaRR <romanyuk@smartdec.ru>
2020-01-13 00:48:21 +0300

    add dependency

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="companies_union",
     packages=find_packages(),
-    install_requires=['PyContracts', 'pandas', 'numpy', 'xlrd', 'cleanco', 'pytest']
+    install_requires=['PyContracts', 'pandas', 'numpy', 'xlrd', 'openpyxl', 'cleanco', 'pytest']
 )
