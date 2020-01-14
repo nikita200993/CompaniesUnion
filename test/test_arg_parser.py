64656e6a3c3883db2b822e9755823221ccef6827
NikitaRR <romanyuk@smartdec.ru>
2020-01-15 02:20:44 +0300

    remove system dependent things

--- test/test_arg_parser.py
+++ test/test_arg_parser.py
@@ -4,14 +4,14 @@ from companies_union.CompanyUnionArgumentParser import CompanyUnionArgumentParse
 class TestArgParser:
 
     def test_1(self):
-        args = ["./ds.xlsx", "kek.xlsx", "-f", "abc"]
+        args = ["ds.xlsx", "kek.xlsx", "-f", "abc"]
         arg_parser = CompanyUnionArgumentParser()
         namespace = arg_parser.parse_args(args)
-        set_of_files_expected = {"./ds.xlsx", "kek.xlsx"}
+        set_of_files_expected = {"ds.xlsx", "kek.xlsx"}
         assert set_of_files_expected == {x for x in namespace.datasets}
 
     def test_2(self):
-        args = ["./ds.xlsx", "kek.xlsx"]
+        args = ["ds.xlsx", "kek.xlsx"]
         arg_parser = CompanyUnionArgumentParser()
         try:
             arg_parser.parse_args(args)
@@ -20,19 +20,19 @@ class TestArgParser:
             pass
 
     def test_3(self):
-        args = ["./ds.xlsx", "kek.xlsx", "-f", "abc"]
+        args = ["ds.xlsx", "kek.xlsx", "-f", "abc"]
         arg_parser = CompanyUnionArgumentParser()
         namespace = arg_parser.parse_args(args)
         assert " ".join(namespace.field_name) == "abc"
 
     def test_4(self):
-        args = ["./ds.xlsx", "kek.xlsx", "-f", "abc kek"]
+        args = ["ds.xlsx", "kek.xlsx", "-f", "abc kek"]
         arg_parser = CompanyUnionArgumentParser()
         namespace = arg_parser.parse_args(args)
         assert " ".join(namespace.field_name).strip() == "abc kek"
 
     def test_5(self):
-        args = ["./ds.xlsx", "kek.xlsx", "-f", "abc kek zZa    ", "-m", "Ab.xlsx"]
+        args = ["ds.xlsx", "kek.xlsx", "-f", "abc kek zZa    ", "-m", "Ab.xlsx"]
         arg_parser = CompanyUnionArgumentParser()
         namespace = arg_parser.parse_args(args)
         assert namespace.mapper == "Ab.xlsx"
