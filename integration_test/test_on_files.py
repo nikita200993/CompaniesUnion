8b40857346f6635b8e93f5fe17cc0cc65be37f90
NikitaRR <romanyuk@smartdec.ru>
2020-01-15 00:41:53 +0300

    write test with real data

--- integration_test/test_on_files.py
+++ integration_test/test_on_files.py
@@ -2,12 +2,16 @@ from companies_union.CompanyUnionArgumentParser import CompanyUnionArgumentParse
 from companies_union.union import get_union_from_files
 
 import os.path as path_utils
+from pandas import read_excel
 
-cmd_line_args = ["1.xlsx", "2.xlsx", "3.xlsx", "4.xlsx", "-f", "company", "name", "-t", "resources", "-m", "./resources/mapper.xlsx"]
+cmd_line_args = ["1.xlsx", "2.xlsx", "3.xlsx", "4.xlsx", "-f", "company", "name", "-t", "resources"]
+result_path = path_utils.join("resources", "result.xlsx")
 
 
 def test_on_real_files():
     args = CompanyUnionArgumentParser().parse_args(cmd_line_args)
     file_names = map(lambda file_name: path_utils.join("resources", file_name), args.datasets)
     field_name = " ".join(args.field_name).strip()
-    get_union_from_files(file_names, field_name, mapper_path=args.mapper, save_path=args.target)
+    actual = get_union_from_files(file_names, field_name, mapper_path=args.mapper, save_path=args.target)
+    expected = read_excel(result_path, header=[0, 1])
+    assert actual.equals(expected)
